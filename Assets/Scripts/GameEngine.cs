3358f21e29ecdbfb38f605ce6d4ec9dbbb1525cc
Spartansean98 <Spartansean98@users.noreply.github.com>
2017-10-04 10:42:45 +0200

    Commit 2nd

---
+++ Assets/Scripts/GameEngine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("fucking memes");
+        Instantiate(Resources.Load("Dragon"),new Vector3(0,0,-1),Quaternion.identity);
+        Instantiate(Resources.Load("Knight"), new Vector3(-4, -3, -1), Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
